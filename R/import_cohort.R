b572a207efc0b82833ad587c6c0a05f9771c5063
Robin-Tobias Jauss <48945982+RJauss@users.noreply.github.com>
2023-10-12 11:35:41 +0200

    Cohort import now supports p.codes and c.codes of variants

--- R/import_cohort.R
+++ R/import_cohort.R
@@ -3,9 +3,11 @@
 #' @description Function to import cohort data (.csv or .tsv) and adding variant frequency and protein position
 #'
 #' @param path_to_cohort filepath to the cohort data, can be .tsv or .csv
-#' @param p_code_column the name of the column that contains the variant's p.code (required)
+#' @param variant_column the name of the column that contains either
+#' the variant's p.code or c.code (the code is specified in the next variable)
+#' @param code either "c" if you provide the variant's c.code or "p" if you provide the variant's p.code
 #'
-#' @return A tibble containing the cohort data with variant frequency and protein positon added
+#' @return A tibble containing the cohort data with variant frequency and protein position added
 #' @export
 #'
 #' @import dplyr
@@ -14,16 +16,27 @@
 #'
 #' @examples
 #' examplepath = system.file("extdata", "MBOAT7_patient_variants.csv", package="LolliplotR")
-#' MBOAT7_cohort = import_cohort(path_to_cohort = examplepath, p_code_column = "Variant_p")
+#' MBOAT7_cohort = import_cohort(path_to_cohort = examplepath, variant_column = "Variant_p", code = "p")
 
-import_cohort = function(path_to_cohort, p_code_column){
+import_cohort = function(path_to_cohort, variant_column, code){
   cohort = as_tibble(fread(path_to_cohort)) %>%
     # count the variants
     # "across" needed because we use a variable from the function
-    add_count(across(p_code_column), name = "Cohort variant occurence") %>%
+    add_count(across(all_of(variant_column)), name = "Cohort variant occurence")
     # extract amino acid position
     # "get" needed because we use a variable from the function
-    mutate(protein_position = as.numeric(str_extract(get(p_code_column), "[0-9]+")))
+    # if c.code is provided, we calculate the protein position manually
+  if(code == "p"){
+    cohort = cohort %>%
+      mutate(protein_position = as.numeric(str_extract(get(variant_column), "[0-9]+")))
+  } else if (code == "c"){
+    cohort = cohort %>%
+      mutate(protein_position = ceiling(as.numeric(str_extract(get(variant_column), "[0-9]+"))/3))
+  } else {
+    stop("You must either provide 'c' for the c.code or 'p' for the p.code")
+  }
+
+
 
   return(cohort)
 }
